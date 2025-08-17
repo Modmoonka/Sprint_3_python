3669d884f11838aefb40f9b5916a7abf15c62489
BabatenkoTO <tanya-mongush@mail.ru>
2025-08-18 00:16:02 +0700

    new change

--- task_1.py
+++ task_1.py
@@ -16,5 +16,31 @@ class OnlineSalesRegisterCollector:
         self.__number_items = 0
         self.__item_price = {'чипсы': 50, 'кола': 100, 'печенье': 45, 'молоко': 55, 'кефир': 70}
         self.__tax_rate = {'чипсы': 20, 'кола': 20, 'печенье': 20, 'молоко': 10, 'кефир': 10}
+    
+    #1. Напиши геттеры
+    @property
+    def name_items(self):
+        return self.__name_items
 
-    ...# допиши код здесь 
+    @property
+    def number_items(self):
+        return self.__number_items
+    
+    #2. Добавь товар в чек
+    def add_item_to_cheque(self, name):
+        if len(name) == 0 or len(name) > 40:
+            raise ValueError('Нельзя добавить товар, если в его названии нет символов или их больше 40')
+        else:
+            if name not in self.__item_price:
+                raise NameError('Позиция отсутствует в товарном справочнике')
+            else:
+                self.__name_items.append(name)
+                self.__number_items += 1
+
+    #3. Удали товар из чека
+    def delete_item_from_check(self, name):
+        if name not in self.__name_items:
+            raise NameError('Позиция отсутствует в чеке')
+        else:
+            self.__name_items.remove(name)
+            self.__number_items -= 1
